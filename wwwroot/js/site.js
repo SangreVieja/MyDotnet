8c7f5bed8db01a1cc3f44590c3c3b870b0c92283
Juan Diego Gómez <jdgv15@hotmail.com>
2023-06-30 11:31:02 -0500

    inicializa

---
+++ wwwroot/js/site.js
@@ -0,0 +1,14 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+function CositaGrave(id1, id2, id3) {
+    
+}
+
+try {
+    
+} catch (error) {
+    
+}
+console.log();
